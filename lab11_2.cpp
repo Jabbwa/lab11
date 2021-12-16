39913258e0fc2848b968806b3b554e582d8abc76
Jabbwa <sopida_kit@cmu.ac.th>
2021-12-16 23:31:40 +0700

    complete

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,6 +9,17 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
+	dest << "-------------------- BOOM ---------------------\n";
+	dest << "AR CHI\n";
+	dest << "AR BOW\n";
+	dest << "CHOW CHOW\n";
+	dest << "BA KA\n";
+	dest << "BA GEAR\n";
+	dest << "CHEER CHEER\n";
+	dest << "WE ARE ALL\n";
+	dest << "ENTANEER\n";
+	dest << "CMU\n";
+	dest << "-------------------- HA!! ---------------------";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
